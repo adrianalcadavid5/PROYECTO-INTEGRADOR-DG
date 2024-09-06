73e172088545ff5d878a0a1c5106a4a0d82da257
Adriana Lucia Cadavid Tabares <adrianalcadavid5@gmail.com>
2024-09-05 22:17:38 -0500

    Se agregaron al proyecto los testing(turno, paciente y odontologo)

--- src/test/java/com/dh/clinica/OdontologoServiceTest.java
+++ src/test/java/com/dh/clinica/OdontologoServiceTest.java
@@ -1,45 +1,44 @@
 package com.dh.clinica;
 
+import com.dh.clinica.entity.Odontologo;
+import com.dh.clinica.service.impl.OdontologoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@Transactional
 class OdontologoServiceTest {
-    /*
-    public static final Logger logger = LoggerFactory.getLogger(OdontologoServiceTest.class);
+    @Autowired
+    OdontologoService odontologoService;
 
-    OdontologoService odontologoService = new OdontologoService(new OdontologoH2Dao());
+    Odontologo odontologo;
+    Odontologo odontologoDesdeDb;
 
-    @BeforeAll
-    static void tablas(){
-        H2Connection.crearTablas();
+    @BeforeEach
+    void crearOdontologo(){
+        odontologo = new Odontologo();
+        odontologo.setNumeroMatricula("1234");
+        odontologo.setNombre("Pedro");
+        odontologo.setApellido("Perez");
+        odontologoDesdeDb = odontologoService.guardarOdontologo(odontologo);
     }
 
     @Test
-    @DisplayName("Testear que un odondologo se guarde en la base de datos")
+    @DisplayName("Testear que un odontologo se guarde en la base de datos")
     void caso1(){
-        // dado
-        Odontologo odontologo = new Odontologo("1234", "Valeria", "Serna");
-        //cuando
-        Odontologo odontologoDesdeDB = odontologoService.guardarOdontologo(odontologo);
-        //enconces
-        assertNotNull(odontologoDesdeDB.getId());
-    }
-
-    @Test
-    @DisplayName("Testear que un odontologo puede ser encontrado cuando se envia un id")
-    void  caso2(){
         //dado
-        Integer id = 1;
         //cuando
-        Odontologo odontologo = odontologoService.buscarPorId(id);
-        //enconces
-        assertEquals(id, odontologo.getId());
+        //entonces
+        assertNotNull(odontologoDesdeDb.getId());
+        assertEquals("1234", odontologoDesdeDb.getNumeroMatricula());
+        assertEquals("Pedro", odontologoDesdeDb.getNombre());
+        assertEquals("Perez", odontologoDesdeDb.getApellido());
     }
-    @Test
-    @DisplayName("Testear que busque todos los odontologos")
-    void  caso3(){
-        Odontologo odontologo1 = new Odontologo("122","pepe","perez");
-        Odontologo odontologo2 = new Odontologo("552","ana","fernandez");
-        odontologoService.guardarOdontologo(odontologo1);
-        odontologoService.guardarOdontologo(odontologo2);
-        odontologoService.buscarTodos();
-    }
-*/
 }
--- src/test/java/com/dh/clinica/PacienteServiceTest.java
+++ src/test/java/com/dh/clinica/PacienteServiceTest.java
@@ -1,40 +1,59 @@
 package com.dh.clinica;
 
+import com.dh.clinica.entity.Domicilio;
+import com.dh.clinica.entity.Paciente;
+import com.dh.clinica.service.impl.PacienteService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@Transactional
 class PacienteServiceTest {
-    /*
-    public static final Logger logger = LoggerFactory.getLogger(PacienteServiceTest.class);
+    @Autowired
+    PacienteService pacienteService;
 
-    //llamo al pacienteSevice y lo vinculo con el pacienteDaoH2 para usarlo en el test
-    PacienteService pacienteService = new PacienteService(new PacienteDaoH2());
+    Paciente paciente;
+    Paciente pacienteDesdeDb;
 
-    @BeforeAll
-    static void tablas(){
-        H2Connection.crearTablas();
+    @BeforeEach
+    void crearPaciente(){
+        Domicilio domicilio = new Domicilio(null,"Falsa",456,"Cipolleti", "Rio Negro");
+        paciente = new Paciente();
+        paciente.setApellido("Romero");
+        paciente.setNombre("Luciana");
+        paciente.setDni("566655");
+        paciente.setFechaIngreso(LocalDate.of(2024,7,16));
+        paciente.setDomicilio(domicilio);
+        pacienteDesdeDb = pacienteService.guardarPaciente(paciente);
     }
-
-
     @Test
     @DisplayName("Testear que un paciente se guarde en la base de datos con su domicilio")
     void caso1(){
-        //dado ...... un paciente, este debe de ir con el constructor sin el id
-        Paciente paciente = new Paciente("Romero","Luciana", "56655", LocalDate.of(2024,7,16),
-                new Domicilio("falsa", 456, "Cipolleti", "Rio Negro"));
-        //cuando  .. llamamos a nuestro servicio PacienteService y hago arriba
-        Paciente pacienteDesdeDB = pacienteService.guardarPaciente(paciente);
-        //entonces   -- probamos si el paciente no tiene id no se guarda en la base de datos
-        assertNotNull(pacienteDesdeDB.getId());
-
+        //dato
+        //cuando
+        //entonces
+        assertNotNull(pacienteDesdeDb.getId());
     }
+
     @Test
-    @DisplayName("Testear que un paciente pueda ser obtenido cuando se envia un id")
+    @DisplayName("Testear que un paciente pueda ser obtenido cuando se envia el id")
     void caso2(){
-       //dado   .. id 1 se que esta guardado en la base datos
-        Integer id = 1;
-       //cuando
-        Paciente paciente = pacienteService.buscarPorId(id);
-       //enconces
-        assertEquals(id, paciente.getId());
+        //dado
+        Integer id  = pacienteDesdeDb.getId();
+        //cuaando
+        Paciente pacienteEncontrado = pacienteService.buscarPorId(id).get();
+        //entonces
+        assertEquals(id, pacienteEncontrado.getId());
     }
-
-     */
 }
